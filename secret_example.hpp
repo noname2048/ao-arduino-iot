0b26254ea1fbbb648c5b8617115f337c462d3ce8
noname2048 <sungwook.csw@gmail.com>
2021-08-07 18:53:10 +0900

    feat: hide secket key

---
+++ secret_example.hpp
@@ -0,0 +1,8 @@
+// this is example of secret.hpp
+// TODO: 1) change values, 2) rename file to secret.hpp
+#include <Arduino.h>
+
+const char UDP_IP[] = "111.222.333.44";
+const int UDP_PORT = -9999;
+const char WIFI_NAME[] = "WHATISLOVE";
+const char WIFI_PASSWORD[] = "000aaabbb?";
