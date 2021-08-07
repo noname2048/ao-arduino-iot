f3233b10b8ec56e94b91a5d0a12f91aca17dc54e
noname2048 <sungwook.csw@gmail.com>
2021-08-07 18:57:37 +0900

    feat: update main ino

--- sketch_fastai.ino
+++ sketch_fastai.ino
@@ -1,11 +1,35 @@
-#include "sensor.h"
+#include "sensor.hpp"
+#include "lcd.hpp"
+#include "wifi.hpp"
+
+int progress = 0;
+char progressBuf[30] = "";
 
 void setup() {
   Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial.println("FASTAPI_UDP START");
   sensorInit();
+  lcdInit();
+  connect();
 }
 
 void loop() {
+  progress += 1;
+  
   delay(3000);
   sensorRead();
+  lcdPrint(0, getSensorDataStr());
+
+  sprintf(progressBuf, "%d", progress);
+  lcdPrint(1, progressBuf);
+
+  sendData(getSensorDataStr());
 }
+
+// AT
+// AT+CWMODE=1
+// AT+CWJAP?
+// AT+CWJAP=
+// AT+CIFSR
+// AT+CIPSTART="UDP"
