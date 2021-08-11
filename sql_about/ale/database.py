e753ddaf9dfadec068aaf9fd559386108ebb4e8b
noname2048 <sungwook.csw@gmail.com>
2021-08-11 18:39:15 +0900

    feat: add remote db

--- sql_about/ale/database.py
+++ sql_about/ale/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import secret
+
 db_url = "postgresql://postgres:example@localhost:15001/iot"
 engine = create_engine(db_url, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+aws_db_url = f"postgresql://{secret.username}:{secret.password}@{secret.endpoint}:{secret.port}/{secret.dbname}"
